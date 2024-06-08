645f25349a64190d64f766dfa34e7301ddfcacee
Zffu <radi0o0@outlook.com>
2024-06-08 10:14:07 +0200

    Added basic assertion into ZUtils

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@
 // Create the core object of the lib. Can be used to access pretty much anything that the lib has.
 const $Z = new Object();
 
-
+/**
+ * Asserts a condition.
+ * @param {*} condition the condition. 
+ * @param {*} message  the message if the condition is false
+ */
+function assert(condition, message) {
+    if(!condition) throw message;
+}
