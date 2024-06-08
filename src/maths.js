cfc58ca5f2b54a88e06aea1426845ba2000f0f41
Zffu <radi0o0@outlook.com>
2024-06-08 10:04:20 +0200

    Added sigma function solver

--- src/maths.js
+++ src/maths.js
@@ -1,4 +1,20 @@
 /**
- * Math Utils of ZUtils
+ * Math Utils of ZUtils. Directly adds useful stuff into the Math vanilla javascript object.
  * @author Zffu
  */
+
+/**
+ * Solves a sigma (aka summation) with the provided equation.
+ * @param {*} start the start bound of the summation
+ * @param {*} end  the end bound of the summation
+ * @param {*} equation the equation to do on the index of the summation
+ */
+Math.sigma = function(start, end, equation) {
+    var sum = 0;
+
+    for(let i = start; i < end; i++) {
+        sum = equation(i);
+    }
+
+    return sum;
+}
